812feffa6248015c8a809dfd8aae7a1c3d7d43b1
Yuban10703 <yuban10703@foxmail.com>
2023-05-29 00:08:08 +0800

    first

---
+++ main.py
@@ -0,0 +1,46 @@
+import httpx
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.get("/pgc/player/web/playurl")
+async def api_playurl(request: Request):
+    # print(request.)
+    url = f"https://api.bilibili.com/pgc/player/web/playurl?{request.query_params}"
+    headers = dict(request.headers.items())
+    headers.pop("host")
+    headers.pop("connection")
+    headers.pop("accept-encoding")
+    # print(headers)
+    async with httpx.AsyncClient(
+            headers=headers,
+            timeout=10,
+    ) as client:
+        res = await client.get(url)
+    # print(res.text)
+    return res.json()
+
+
+@app.get("/pgc/player/api/playurl")
+async def api_playurl(request: Request):
+    # print(request.)
+    url = f"https://api.bilibili.com/pgc/player/api/playurl?{request.query_params}"
+    headers = dict(request.headers.items())
+    headers.pop("host")
+    headers.pop("connection")
+    headers.pop("accept-encoding")
+    # print(headers)
+    async with httpx.AsyncClient(
+            headers=headers,
+            timeout=10,
+    ) as client:
+        res = await client.get(url)
+    # print(res.text)
+    return res.json()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("chuchai:app", host="0.0.0.0", port=9999, reload=True)
